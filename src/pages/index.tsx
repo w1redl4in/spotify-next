236b1a1d8e93637fa35eb494ade734f26bec5d8f
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 18:22:43 -0300

    SSG for featured playlists :sparkles:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,22 +3,31 @@ import { Content } from '../components/Content';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import { Sidebar } from '../components/Sidebar';
+import { api } from '../services/api';
+import { IFeaturedPlaylists } from '../types/featuredPlaylists';
 
-export default function Home() {
+export default function Home({ message, playlists }: IFeaturedPlaylists) {
   return (
     <>
       <Sidebar />
       <Header />
-      <Content />
+      <Content message={message} playlists={playlists} />
       <Player />
     </>
   );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get(
+    'browse/featured-playlists?country=BR&locale=pt_BR'
+  );
+
+  const { message, playlists } = response.data;
+
   return {
     props: {
-      a: 'a',
+      message,
+      playlists,
     },
   };
 };
