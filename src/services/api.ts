1e75619f970777fffe1395d064403ecc9ddd49fb
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 20:51:13 -0300

    fix: deleting axios interceptor :fire:

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,10 +3,3 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: `https://api.spotify.com/v1/`,
 });
-
-api.interceptors.request.use((config) => {
-  config.headers = {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_OAUTH_TOKEN}`,
-  };
-  return config;
-});
