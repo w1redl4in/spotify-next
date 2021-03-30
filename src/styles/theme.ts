d62e24d4f7cafd1d2fb9e91d5ef4ec636a79fb69
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 00:48:34 -0300

    chore: theme :lipstick:

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    black: '#000000',
+    grey: '#1D1D1D',
+    darkGrey: '#0F0F0F',
+    lightGrey: '#282828',
+    white: '#FFFFFF',
+    spotify: '#1ED760',
+  },
+  fontSize: {
+    huge: '18px',
+    large: '16px',
+    medium: '14px',
+    small: '12px',
+    tiny: '10px',
+  },
+};
+
+export type ITheme = typeof theme;
