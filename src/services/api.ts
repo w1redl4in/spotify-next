126a307fa6e30f4afc90dbf402c3c3db52a00ee2
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 18:21:04 -0300

    chore: configuring header token, gotta fix that aswell :bug:

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: `https://tastedive.com/api`,
+  baseURL: `https://api.spotify.com/v1/`,
 });
 
 api.interceptors.request.use((config) => {
-  config.params = { k: `${process.env.NEXT_PUBLIC_TASTE_DIVE_API_KEY}` };
+  config.headers = {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_OAUTH_TOKEN}`,
+  };
   return config;
 });
