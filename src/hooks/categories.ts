e4b81869074b23fd2b8b14798e442e5e73f48de8
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 20:51:35 -0300

    refactor: headers and trycatch :recycle:

--- src/hooks/categories.ts
+++ src/hooks/categories.ts
@@ -1,13 +1,18 @@
 import { api } from '../services/api';
 
 export async function useCategories(accessToken: string | undefined) {
-  const response = await api.get<any>(
-    'browse/categories?country=BR&locale=pt_BR',
-    {
-      headers: {
-        Authorization: accessToken,
-      },
-    }
-  );
-  return response.data;
+  console.log('token', accessToken);
+  try {
+    const response = await api.get<any>(
+      'browse/categories?country=BR&locale=pt_BR',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/hooks/featuredPlaylists.ts
+++ src/hooks/featuredPlaylists.ts
@@ -2,13 +2,17 @@ import { IFeaturedPlaylists } from '../@types/featuredPlaylists';
 import { api } from '../services/api';
 
 export async function useFeaturedPlaylists(accessToken: string | undefined) {
-  const response = await api.get<IFeaturedPlaylists>(
-    'browse/featured-playlists',
-    {
-      headers: {
-        Authorization: accessToken,
-      },
-    }
-  );
-  return response.data;
+  try {
+    const response = await api.get<IFeaturedPlaylists>(
+      'browse/featured-playlists?country=BR&locale=pt_BR',
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
