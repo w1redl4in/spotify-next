4143b4827b1ced70fa26bd82d6fe93930411cecf
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 19:38:49 -0300

    refactor: request featured playlists on client side :recycle:

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,19 +1,39 @@
-import { IItem } from '../../types/featuredPlaylists';
+import { useSession } from 'next-auth/client';
+import { useCallback, useEffect, useState } from 'react';
+import { IFeaturedPlaylists } from '../../@types/featuredPlaylists';
+import { api } from '../../services/api';
 import { ContentContainer, Playlists } from './styles';
 
-type ContentProps = {
-  message: string;
-  playlists: {
-    items: IItem[];
-  };
-};
+export const Content: React.FC = () => {
+  const [session] = useSession();
 
-export const Content: React.FC<ContentProps> = ({ message, playlists }) => {
+  const [
+    featuredPlaylists,
+    setFeaturedPlaylists,
+  ] = useState<IFeaturedPlaylists>();
+
+  const handleFeaturedPlaylists = useCallback(async () => {
+    try {
+      const response = await api.get('browse/featured-playlists', {
+        headers: {
+          Authorization: session?.accessToken,
+        },
+      });
+      setFeaturedPlaylists(response.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }, []);
+
+  useEffect(() => {
+    handleFeaturedPlaylists();
+  }, []);
+
   return (
     <ContentContainer>
-      <h1>{message}</h1>
+      <h1>{featuredPlaylists?.message}</h1>
       <Playlists>
-        {playlists.items.map((item) => (
+        {featuredPlaylists?.playlists?.items.map((item) => (
           <div>
             <img src={item.images[0].url} alt="images" />
             <h2>{item.name}</h2>
