387b2cd05b713fbd0e6e9eed182e240ea63302d7
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 19:38:18 -0300

    refactor: removing SSG :fire:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,15 @@
-import { GetStaticProps } from 'next';
 import { Content } from '../components/Content';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import { Sidebar } from '../components/Sidebar';
-import { api } from '../services/api';
-import { IFeaturedPlaylists } from '../types/featuredPlaylists';
 
-export default function Home({ message, playlists }: IFeaturedPlaylists) {
+export default function Home() {
   return (
     <>
       <Sidebar />
       <Header />
-      <Content message={message} playlists={playlists} />
+      <Content />
       <Player />
     </>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const response = await api.get(
-    'browse/featured-playlists?country=BR&locale=pt_BR'
-  );
-
-  const { message, playlists } = response.data;
-
-  return {
-    props: {
-      message,
-      playlists,
-    },
-  };
-};
