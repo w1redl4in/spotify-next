4bae3f5818d813f95f470881f5ee68a9f3d35c7e
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 18:21:54 -0300

    feat: receiving jwt token :sparkles:

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,6 +6,20 @@ export default NextAuth({
     Providers.Spotify({
       clientId: String(process.env.SPOTIFY_API_KEY),
       clientSecret: String(process.env.SPOTIFY_SECRET),
+      scope: 'user-library-read',
     }),
   ],
+  callbacks: {
+    async jwt(token, _, account, profile) {
+      if (account) {
+        token.id = account.id;
+        token.accessToken = account.accessToken;
+      }
+      return token;
+    },
+    async session(session: any, user) {
+      session.user = user;
+      return session;
+    },
+  },
 });
