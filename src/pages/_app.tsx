da16375752f64ed0c7456c2b5b2880a8170f67f5
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 16:15:25 -0300

    feat: next auth provider :sparkles:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,23 +3,26 @@ import Head from 'next/head';
 import { ThemeProvider } from 'styled-components';
 import { Layout } from '../layout';
 import { GlobalStyles, theme } from '../styles';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Spotify - Web Player</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <GlobalStyles />
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ThemeProvider>
+    <NextAuthProvider session={pageProps.session}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Spotify - Web Player</title>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <GlobalStyles />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </NextAuthProvider>
   );
 }
 
