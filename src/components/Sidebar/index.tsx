472695a07e7b3dfb9be1c190731a0b12322d0a64
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 00:50:17 -0300

    feat: sidebar component :tada:

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,5 @@
+import { SidebarContainer } from './styles';
+
+export const Sidebar: React.FC = () => {
+  return <SidebarContainer>a</SidebarContainer>;
+};
---
+++ src/components/Sidebar/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const SidebarContainer = styled.section`
+  grid-area: Sidebar;
+  background: red;
+`;
