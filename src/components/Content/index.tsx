a9a3b07e5a3e11bb366900ad76b914a405419eef
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 18:23:07 -0300

    chore: started content component with its integration :sparkles:

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,5 +1,26 @@
-import { ContentContainer } from './styles';
+import { IItem } from '../../types/featuredPlaylists';
+import { ContentContainer, Playlists } from './styles';
 
-export const Content: React.FC = () => {
-  return <ContentContainer>b</ContentContainer>;
+type ContentProps = {
+  message: string;
+  playlists: {
+    items: IItem[];
+  };
+};
+
+export const Content: React.FC<ContentProps> = ({ message, playlists }) => {
+  return (
+    <ContentContainer>
+      <h1>{message}</h1>
+      <Playlists>
+        {playlists.items.map((item) => (
+          <div>
+            <img src={item.images[0].url} alt="images" />
+            <h2>{item.name}</h2>
+            <p>{item.owner.display_name}</p>
+          </div>
+        ))}
+      </Playlists>
+    </ContentContainer>
+  );
 };
--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -2,5 +2,51 @@ import styled from 'styled-components';
 
 export const ContentContainer = styled.main`
   grid-area: Content;
-  background: ${({ theme }) => theme.colors.lightGrey};
+  background: ${({ theme }) => theme.colors.anotherGrey};
+
+  h1 {
+    color: ${({ theme }) => theme.colors.white};
+    margin: 1rem;
+  }
+`;
+
+export const Playlists = styled.section`
+  display: flex;
+
+  > div {
+    max-width: 10rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    padding: 0.8rem;
+    margin: 0.5rem 1rem;
+    border-radius: 0.5rem;
+    background: ${({ theme }) => theme.colors.someGrey};
+
+    transition: transform 0.2s;
+
+    &:hover {
+      cursor: pointer;
+      transform: scale(1.1);
+    }
+
+    h2 {
+      color: ${({ theme }) => theme.colors.white};
+      font-size: ${({ theme }) => theme.fontSize.large};
+      margin-top: 1rem;
+    }
+
+    p {
+      color: ${({ theme }) => theme.colors.grey99};
+      font-size: ${({ theme }) => theme.fontSize.medium};
+      margin-top: 1rem;
+    }
+  }
+
+  img {
+    width: 8rem;
+    height: 8rem;
+    border-radius: 1rem;
+  }
 `;
