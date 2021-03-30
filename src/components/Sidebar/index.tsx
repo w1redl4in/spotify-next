f47feecef6429e216bf8e972879795d4caea933f
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 02:11:45 -0300

    feat: sidebar component :tada: :sparkles:

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,5 +1,42 @@
-import { SidebarContainer } from './styles';
+import { NAVBAR1 } from '../../constants/Navbar1';
+import { NAVBAR2 } from '../../constants/Navbar2';
+import {
+  SidebarContainer,
+  AdditionalInfo,
+  NavItems,
+  InstallAppLabel,
+} from './styles';
+import { FiArrowDownCircle } from 'react-icons/fi';
 
 export const Sidebar: React.FC = () => {
-  return <SidebarContainer>a</SidebarContainer>;
+  return (
+    <SidebarContainer>
+      <nav>
+        <h1>
+          <img
+            src="https://cdn.discordapp.com/attachments/263762535763673089/826307989384200192/unknown.png"
+            alt="spotify logo"
+          />
+        </h1>
+        <NavItems>
+          {Object.entries(NAVBAR1).map(([key, value]) => (
+            <a href="#" key={key} className={value.className}>
+              {value.icon} {key}
+            </a>
+          ))}
+        </NavItems>
+        <AdditionalInfo>
+          {Object.entries(NAVBAR2).map(([key, value]) => (
+            <span key={key}>
+              {value.icon} {key}
+            </span>
+          ))}
+        </AdditionalInfo>
+      </nav>
+      <InstallAppLabel>
+        <FiArrowDownCircle />
+        <span>Install App</span>
+      </InstallAppLabel>
+    </SidebarContainer>
+  );
 };
--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -2,5 +2,107 @@ import styled from 'styled-components';
 
 export const SidebarContainer = styled.section`
   grid-area: Sidebar;
-  background: red;
+  background: ${({ theme }) => theme.colors.black};
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  nav {
+    height: 18rem;
+    padding: 0.8rem 0;
+    color: ${({ theme }) => theme.colors.lighterGrey};
+
+    h1 {
+      display: flex;
+      align-items: center;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+`;
+
+export const NavItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0 0.1rem;
+  justify-content: space-around;
+  height: 8rem;
+  margin-top: 1rem;
+
+  a {
+    display: flex;
+    align-items: center;
+    font-size: ${({ theme }) => theme.fontSize.medium};
+    width: 98%;
+    padding: 0.6rem 1rem;
+    font-weight: bold;
+
+    transition: color 0.2s;
+
+    &.active {
+      background: ${({ theme }) => theme.colors.grey};
+      border-radius: 0.2rem;
+      color: ${({ theme }) => theme.colors.white};
+    }
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.white};
+    }
+
+    svg {
+      width: 1.5rem;
+      height: 1.5rem;
+      margin-right: 1rem;
+    }
+  }
+`;
+
+export const AdditionalInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-top: 2rem;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grey};
+
+  span {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem 1rem;
+    transition: color 0.2s;
+    font-weight: bold;
+
+    &:hover {
+      color: ${({ theme }) => theme.colors.white};
+      cursor: pointer;
+    }
+
+    svg {
+      width: 1.5rem;
+      height: 1.5rem;
+      margin-right: 1rem;
+    }
+  }
+`;
+
+export const InstallAppLabel = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 1rem 2rem;
+  color: ${({ theme }) => theme.colors.lighterGrey};
+  transition: color 0.2s;
+
+  &:hover {
+    cursor: pointer;
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-right: 0.5rem;
+  }
 `;
