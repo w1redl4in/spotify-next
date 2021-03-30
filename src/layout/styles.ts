6d809619fe03a8cef39cd85f19003140ea4cb8fe
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-30 13:28:55 -0300

    refactor: adding a new line on display grid :recycle:

--- src/layout/styles.ts
+++ src/layout/styles.ts
@@ -3,8 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   grid-template-areas:
+    'Sidebar Header'
     'Sidebar Content'
     'Player Player';
   grid-template-columns: 0.7fr 5fr;
-  grid-template-rows: 85vh 15vh;
+  grid-template-rows: 5vh 80vh 15vh;
 `;
