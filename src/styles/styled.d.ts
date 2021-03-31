2e090074cbf26d4927085edac0867494aaa2230e
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-31 01:00:16 -0300

    fix: theme type checking :recycle:

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,5 +1,5 @@
 import 'styled-components';
-import { ITheme } from './';
+import { ITheme } from './theme';
 
 declare module 'styled-components' {
   export interface DefaultTheme extends ITheme {}
